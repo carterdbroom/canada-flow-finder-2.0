f26d40ff81c099123d925f5ab012110eb5f5593a
Carter Broom <146979015+carterdbroom@users.noreply.github.com>
2025-07-01 20:18:18 -0400

    Downloading data with selenium now.

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # Canada Flow Finder 2.0
-A fully fledged app based on the first Canada Flow Finder
+### A fully fledged app based on the first Canada Flow Finder
+
 ---
 ### Day 1
 Today I planned out what I wanted this project to be and what I wanted to accomplish with it. I want to have an active userbase for this app. If want that, I need to make some changes to Flow Finder 1.0. For the first one, I used an awesome API that allows me to have over 200 requests per day, which is nice for a small command line tool that I can use for myself, but that won't cut it for hundreds, if not thousands of users. I checked out the Government of Canada's [Water Office](https://wateroffice.ec.gc.ca/) site, which is where the data comes from. I realized I could do some web scraping to get the data for each station. I started some programming and used the API from [SCRAPI](https://scrap2api.web.app/) to get a list of every station ID since I will need to modify the download URL for each station to get the data. I also have decided on what tech stack I am going to use.
@@ -7,3 +8,7 @@ Today I planned out what I wanted this project to be and what I wanted to accomp
 ---
 ### Day 2
 I started working on the web scraper to get the csv data. I learned lots on Selenium, a super cool web scraping technology. I played around with it a bit and started making some progress towards downloading the csv file for each station. Tomorrow I should have the web scraping component fully functional. 
+
+--- 
+### Day 3
+I made lots of progress, learned about how to navigate through HTML elements with XPath. I am now able to download the data for each river station. I had to work on some error handling since not every station has discharge data. One concern that I am starting to have is the amount of time it will take to download all the data. I haven't even started reading the csv data, and it still takes roughly 7 to 8 seconds for every station. There are over 2000 stations on the website, meaning it will take around 14 to 16 thousand seconds to just download all the data. This means I will only be able to update the gauges on the app every 4 or 5 hours. I can get rid of the stations that don't have discharge data, but there aren't many. I may have to rethink how to approach this problem. I think I can use the requests library to make HTTP requests and simulate cookies so I don't have to do as many clicks. Right now, the site uses cookies to know what data to give me when I go to the generic download page. Right now I'm literally clicking through to each station page to "set" the cookies for each station. I'll figure out how to fix this issue tomorrow. 
--- scraper.py
+++ scraper.py
@@ -1,9 +1,13 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException
 
 driver = webdriver.Firefox()
 station_ids = []
+stations_with_no_data = []
 
 # Reads station IDs from a text file and stores them in a list
 with open ('stations.txt', 'r') as f:
@@ -20,6 +24,17 @@ agreement_button.click()
 download_link = driver.find_element(By.LINK_TEXT, 'Download?')
 download_link.click()  
 
+try:
+    # Finds the csv discharge link and clicks it
+    # Note: The XPATH may need to be adjusted if the structure of the page changes
+    correct_main = driver.find_element(By.XPATH, '//main[@property="mainContentOfPage"]')
+    correct_div = correct_main.find_element(By.XPATH, './div[2]')
+    correct_section = correct_div.find_element(By.XPATH, './section[2]')
+    discharge_link = correct_section.find_element(By.XPATH, './/a[contains(@href, "report_e.html") and contains(@href, "df=csv")]')
+    discharge_link.click()
+except TimeoutException:
+    print(f'Cannot find discharge data for station: {station_ids[0]}')
+    stations_with_no_data.append(station_ids[0])
 # Opens the Water Office website to download data for each station ID
 for i in range(1, len(station_ids)):
     # Navigates to the station's page
@@ -28,8 +43,22 @@ for i in range(1, len(station_ids)):
     # Finds the download link by its text and clicks it
     download_link = driver.find_element(By.LINK_TEXT, 'Download?')
     download_link.click()  
+    
+    # In case there isn't data for the discharge, we check for the presence of the discharge header
+    try: 
+        # Finds the csv discharge link and clicks it
+        # Note: The XPATH may need to be adjusted if the structure of the page changes
+        correct_section = WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.XPATH, '//*[contains(text(), "Discharge (unit values)")]')))
 
-    # Finds the correct discharge header
-    #discharge_header = driver.find_element(By.XPATH, '//h2[contains(text(), "Discharge (unit values)")]')
+        correct_main = driver.find_element(By.XPATH, '//main[@property="mainContentOfPage"]')
+        correct_div = correct_main.find_element(By.XPATH, './div[2]')
+        correct_section = correct_div.find_element(By.XPATH, './section[2]')
+        discharge_link = correct_section.find_element(By.XPATH, './/a[@href="/download/report_e.html?dt=47&df=csv&ext=zip"]')
+        discharge_link.click()
+    
+    except TimeoutException:
+        print(f'Cannot find discharge data for station: {station_ids[i]}')
+        stations_with_no_data.append(station_ids[i])
+        continue    
 
-    #discharge_csv_link = discharge_header.find_element(By.XPATH, 
+driver.close()
